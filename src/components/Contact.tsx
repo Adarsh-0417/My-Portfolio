3319b29a2b4c28896a31a08729fdfc4501548d14
adarsh-0417 <adarshsharma0417@gmail.com>
2026-02-20 02:54:41 +0530

    connected frontend to deployed backend again

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -23,7 +23,7 @@ const Contact = () => {
   setIsSubmitting(true);
 
   try {
-    const response = await fetch("https://portfolio-backend.onrender.com/api/contact", {
+    const response = await fetch("https://portfolio-backend-z98t.onrender.com//api/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
